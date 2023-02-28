f5796046de8d29fd995dd17a0a4c8723c13070cd
BScruggs95 <96084424+BScruggs95@users.noreply.github.com>
2023-02-27 22:34:11 -0500

    pr

---
+++ js_practice/js/app.js
@@ -0,0 +1,35 @@
+// // Console Log
+// console.log("Yo")
+
+// // Alert
+// // alert("annoying")
+
+// // document.getElementById("demo").style.color = "crimson";
+// let demo = document.getElementById("demo");
+
+// demo.innerHTML = "La Vida es Una!";
+
+// demo.style.color = "crimson"
+
+// document.getElementsByClassName("total")
+
+// let total = document.getElementsByClassName("total")
+
+// total.style.color = "cyan"
+
+// Write a fuction to greet
+// Fuction Decleration
+function greet() {
+    alert("Hello");
+}
+// Calling Fuction (Turn on Washing Machine)
+// greet();
+
+// // Square a Number
+// function square(number) {
+//     return number * number;
+// }
+
+// // Save value in HTML
+// let result = square(525);
+// document.getElementById("demo").innerHTML = result;
