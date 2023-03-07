a1d7905f0dd61f434bf577bc7c47116af43ae7ca
BScruggs95 <96084424+BScruggs95@users.noreply.github.com>
2023-03-06 19:10:29 -0500

    lb - pt = 3

--- 105_Lightbox_template/js/lightbox.js
+++ 105_Lightbox_template/js/lightbox.js
@@ -14,20 +14,34 @@ function openLightbox() {
 }
 
 // Reference Images & Opening Lightbox
-let coffeeImage = document.getElementById("coffeeImage");
-coffeeImage.onclick = openLightbox;
-let cornImage = document.getElementById("cornImage");
-cornImage.onclick = openLightbox;
-let columbianImage = document.getElementById("columbianImage");
-columbianImage.onclick = openLightbox;
-let sunsetImage = document.getElementById("sunsetImage");
-sunsetImage.onclick = openLightbox;
+// let coffeeImage = document.getElementById("coffeeImage");
+// coffeeImage.onclick = openLightbox;
+// let cornImage = document.getElementById("cornImage");
+// cornImage.onclick = openLightbox;
+// let columbianImage = document.getElementById("columbianImage");
+// columbianImage.onclick = openLightbox;
+// let sunsetImage = document.getElementById("sunsetImage");
+// sunsetImage.onclick = openLightbox;
+let = galleryImages = document.getElementsByClassName("galleryImage");
+
+// Looping (Open)
+ for (let i = 0; i < galleryImages.length; i++) {
+    let galleryImage = galleryImages[i];
+    galleryImage.onclick = openLightbox;
+ }
 
 
 // Closing Lightbox + Trigger
 function closeLightbox() {
     lightboxContainer.classList.remove("display");
     lightbox.src = "";
+}
+
+// Looping (Close)
+let closers = document.getElementsByClassName('closer');
+for (let i = 0; i < closers.length; i++) {
+    let closer = closers[i];
+    closer.onclick = closeLightbox;
 }
 
 lightboxBackground.onclick = closeLightbox;
