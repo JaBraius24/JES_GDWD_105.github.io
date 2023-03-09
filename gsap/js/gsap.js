e99ae4d232cbdd92a86a0b2fe364d86c482254da
BScruggs95 <96084424+BScruggs95@users.noreply.github.com>
2023-03-08 22:14:36 -0500

    gsap-prcte

---
+++ gsap/js/gsap.js
@@ -0,0 +1,17 @@
+// Box Animation
+gsap.from(".chucked",{
+    duration: 5,
+    ease: "elastic",
+    rotation: 720,
+    x: -500,
+    y: -500,
+    stagger: 0.5,
+});
+
+// Text Animation
+gsap.from(".headline",{
+    duration: 2.5,
+    opacity: 0,
+    y: 300,
+    stagger: 0.3,
+});
